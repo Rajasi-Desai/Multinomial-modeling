8bd0bb71d91de622f4e4a5b180e02f4d778c9793
Rajasi Desai <rajasi.desai18@gmail.com>
2022-04-12 15:34:31 -0400

    added basic structure

---
+++ model.py
@@ -0,0 +1,6 @@
+#n = number of trials
+#k = number of mutually exclusive events
+#alpha_values = list of probabilities [p1,p2.....pk] such that their sum is 1
+
+def model(n, k, alpha_values):
+    print("check")
